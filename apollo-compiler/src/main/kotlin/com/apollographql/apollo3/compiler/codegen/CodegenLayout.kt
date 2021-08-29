143d2959675964ea12801cf5b210f7ba2f2e9074
Martin Bonnin <martin@mbonnin.net>
2021-08-29 14:17:57 +0200

    remove unused code

--- apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/codegen/CodegenLayout.kt
+++ apollo-compiler/src/main/kotlin/com/apollographql/apollo3/compiler/codegen/CodegenLayout.kt
@@ -90,7 +90,6 @@ class CodegenLayout(
   internal fun variableName(name: String) = regularIdentifier(name)
   internal fun propertyName(name: String) = regularIdentifier(name)
   internal fun typesName() = "Types"
-  internal fun cacheResolverName() = "ApolloCacheResolver"
 
   // ------------------------ Helpers ---------------------------------
   private fun regularIdentifier(name: String) = name.escapeKotlinReservedWord()
